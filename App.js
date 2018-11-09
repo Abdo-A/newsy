e00c595962a95da1360b9841961d170f4b80c0bf
Abdo <abdopoetry@gmail.com>
2018-11-09 14:57:47 +0200

    First commit - Built app skeleton

---
+++ App.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+import Newsy from "./src/Newsy";
+
+export default class App extends React.Component {
+  render() {
+    return <Newsy />;
+  }
+}
---
+++ src/Newsy.js
@@ -0,0 +1,8 @@
+import React, { Component } from "react";
+import { Text } from "react-native";
+
+export default class Newsy extends Component {
+  render() {
+    return <Text>Newsy App</Text>;
+  }
+}
